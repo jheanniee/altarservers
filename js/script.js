fc4c3601aeaec792cddc41eb8afafdbbe64da115
jheanniee <71245287+jheanniee@users.noreply.github.com>
2022-08-03 16:04:32 +0800

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-document.querySelectorAll('.about .image .controls .control-btn').forEach(btn =>{
-    btn.onclick = () =>{
+document.querySelectorAll('.about .image .controls .control-btn').forEach(btn => {
+    btn.onclick = () => {
         let src = btn.getAttribute('data-src');
         document.querySelector('.about .image .image').src = src;
     }
 })
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
